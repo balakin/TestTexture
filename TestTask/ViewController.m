d16117e0174b3bb8df631ae152eee1d5d2fc23d3
Andrew <andrew@MacBook-Pro-Andrew.local>
2018-06-25 16:56:13 +0300

    Reactive finished

--- TestTask/ViewController.m
+++ TestTask/ViewController.m
@@ -32,9 +32,39 @@
     RACSignal * signal3 = [[RACSignal return:@3] delay:1];
     RACSignal * signal4 = [[RACSignal return:@4] delay:1];
     RACSignal * signal5 = [[RACSignal return:@5] delay:1];
+    
     // требуется сделать 2 сигнала паралельно, потом 1 последовательно, 2 паралельно
 
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [[RACSignal
+      combineLatest:@[signal1, signal2]
+      reduce:^id(NSNumber *number1, NSNumber *number2)
+    {
+          return [NSNumber numberWithBool:[number1 intValue] + [number2 intValue] == 3];
+    }]
+    subscribeNext:^(NSNumber *completed12)
+    {
+        if ([completed12 intValue] > 0)
+        [signal3 subscribeNext:^(NSNumber *number3)
+        {
+            if ([number3 intValue] == 3)
+            {
+                [[RACSignal
+                  combineLatest:@[signal4, signal5]
+                  reduce:^id(NSNumber *number4, NSNumber *number5)
+                  {
+                      return [NSNumber numberWithBool:[number4 intValue] + [number5 intValue] == 9];
+                  }]
+                 subscribeNext:^(NSNumber *completed45)
+                 {
+                     if ([completed45 intValue] > 0)
+                     NSLog(@"Finished!!!");
+                 }];
+            }
+        }];
+    }];
+
 }
 
 
